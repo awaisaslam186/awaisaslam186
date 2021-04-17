b29f42a3f9dcaeb2399214c7e55846c38e6e6d0c
Muhammad Awais Shah <82607059+awaisaslam186@users.noreply.github.com>
2021-04-17 13:04:20 +0500

    dfjks

--- deploy.sh
+++ deploy.sh
@@ -1,52 +1,23 @@
-#!/bin/sh
-
-############
-# The following script is to help automate deployment of hugo sites on github pages. 
-# If you're just getting started, I recommend reading my full notes here: 
-# https://www.romandesign.co/setting-up-a-hugo-static-site-on-github/
-# 
-# To run it, make sure you have made some site updates to deploy and via terminal in your backend and save the code in your hugo backend repo. 
-# Then type the following (no $):
-#
-# 	$ chmod +x deploy.sh  #### This will make your script executable. You only need to type this once to modify permissions.
-#    	$ ./deploy.sh "Your optional commit message"   #### This will actually run the code. 
-#
-############
+#!/usr/bin/env bash -xe
 
 
-# If a command fails then the deploy stops
-set -e
+#allow passing a commit message
+message=$1
 
-# Just a tooltip to let you know the script is running.
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n" 
-
-# Build the hugo site to both of your repos
-hugo
-hugo -d ../yourgithubusername.github.io # This builds the public site into your public repo. !!!!! Make sure to replace with your currect public repo here
-
-# Deploy your public site
-cd ../yourgithubusername.github.io # Goes to your public site repo
-git init # Initiatilizes git
-git add . # Adds changes
-
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
+if [ "$message" == "" ]; then
+  #use last git commit message for deploying into github pages
+  message=`git log -1 --pretty=%B`
 fi
 
-git commit -m "$msg" # Makes your commit with the message from your deploy script execution
-git push origin master # Pushes the code to GitHub
+#generate static site into public & copy files into dist folder
+hugo -d public
+rsync -avh --delete public/* dist/
 
-# Now deploy your backend with all of the 
-cd ../hugosite_backend #!!!!! Make sure to replace with your currect backend repo here.
-git add . # Adds changes
+# push the files into github pages
+cd dist
+git add .
+git commit -m "$message"
+git push origin master #replace "master" with "gh-pages" if you are deploying into a project site
 
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-
-git commit -m "$msg" # Makes your commit with the message from your deploy script execution
-git push origin master # Pushes the code to GitHub
+# go back to source directory
+cd -
